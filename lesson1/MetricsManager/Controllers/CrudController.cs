3f5f0bbd36fb4ee1a75bb880a279dd071fbd5adf
Виктория Шумская <overveg@gmail.com>
2021-06-09 15:12:33 +0300

    дз№1

---
+++ lesson1/MetricsManager/Controllers/CrudController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/crud")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        private readonly ValuesHolder _holder;
+
+        public CrudController(ValuesHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] DateTime date, int temperature)
+        {
+            _holder.Add(date, temperature);
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read(DateTime fromDate, DateTime toDate)
+        {
+            return Ok(_holder.Get(fromDate, toDate));
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] DateTime date, int temperature)
+        {
+            _holder.Update(date, temperature);
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime date)
+        {
+            _holder.Delete(date);
+            return Ok();
+        }
+    }
+}
---
+++ lesson1/MetricsManager/Controllers/ValuesHolder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MetricsManager.Controllers
+{
+    public class ValuesHolder
+    {
+        public List<WeatherForecast> WeatherForecasts { get; set; }
+
+        public ValuesHolder() {
+            WeatherForecasts = new List<WeatherForecast>();
+        }
+
+        public WeatherForecast Find(DateTime date)
+        {
+            return WeatherForecasts.FirstOrDefault(p => p.Date == date);
+        }
+
+        public List<WeatherForecast> Get(DateTime fromDate, DateTime toDate) {
+            return WeatherForecasts.Where(p => p.Date >= fromDate && p.Date <= toDate).ToList();
+        }
+
+        public void Add(DateTime date, int temperature) {
+            WeatherForecasts.Add(new WeatherForecast { 
+                Date = date,
+                TemperatureC = temperature
+            });
+        }
+
+        public void Update(DateTime date, int temperature)
+        {
+            var weatherForecast = Find(date);
+            weatherForecast.TemperatureC = temperature;
+        }
+
+        public void Delete(DateTime date)
+        {
+            var weatherForecast = Find(date);
+            WeatherForecasts.Remove(weatherForecast);
+        }
+    }
+}
